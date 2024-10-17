400f871d9ad52e55750e660b1fba92d15b639923
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-10-17 17:18:13 +0600

    openweather-api types pseudonyms added

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,4 +1,6 @@
 import { CountryInfoBrief, CountryInfoDetailed } from './lib/restcountries-api';
+import { WeatherInfo } from './lib/openweather-api';
 
 export type CountryInfoBrief = CountryInfoBrief;
 export type CountryInfoDetailed = CountryInfoDetailed;
+export type WeatherInfo = WeatherInfo;
