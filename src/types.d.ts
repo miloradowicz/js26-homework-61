ac123df62110455d2dabeaa73ee90f64ca4ea8df
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-10-17 16:21:21 +0600

    api types pseudonyms for internal use added

---
+++ src/types.d.ts
@@ -0,0 +1,4 @@
+import { CountryInfoBrief, CountryInfoDetailed } from './lib/restcountries-api';
+
+export type CountryInfoBrief = CountryInfoBrief;
+export type CountryInfoDetailed = CountryInfoDetailed;
